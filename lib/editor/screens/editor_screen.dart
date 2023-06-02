0be8f0aa00e40c5b86f39bc8f8fec2405e6d175b
savinqkr <savinqkr@gmail.com>
2023-06-02 16:35:55 +0900

    feat: Divide Editor section

--- lib/editor/screens/editor_screen.dart
+++ lib/editor/screens/editor_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ojt_test_2/common/templates/layout_template.dart';
 import 'package:ojt_test_2/common/widgets/sidebar_menu/sidebar_menu.dart';
+import 'package:ojt_test_2/config/palette.dart';
 import 'package:ojt_test_2/enums/menu.dart';
 
 class EditorScreen extends StatelessWidget {
@@ -25,12 +26,29 @@ class EditorScreen extends StatelessWidget {
         ),
       ),
       body: LayoutTemplate(
-        menuWidget: const SidebarMenu(currentMenu: menuType),
-        contentWidget: Container(
-          color: Colors.lightBlueAccent,
-          child: const Center(child: Text("EDITOR")),
-        ),
-      ),
+          menuWidget: const SidebarMenu(currentMenu: menuType),
+          contentWidget: Row(
+            children: [
+              Container(
+                width: 200,
+                color: Palette.mint.withOpacity(0.7),
+              ),
+              Container(
+                width: 160,
+                color: Palette.mint.withOpacity(0.3),
+              ),
+              Expanded(
+                child: Container(
+                  width: 200,
+                  color: Palette.white,
+                ),
+              ),
+              Container(
+                width: 300,
+                color: Palette.mint.withOpacity(0.6),
+              ),
+            ],
+          )),
     );
   }
 }
