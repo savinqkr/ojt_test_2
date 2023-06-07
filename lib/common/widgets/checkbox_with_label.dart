89550e75813ea674698fcf7a348af9f6a8d34d84
savinqkr <savinqkr@gmail.com>
2023-06-07 13:53:56 +0900

    feat: TextField with Calendar

---
+++ lib/common/widgets/checkbox_with_label.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ojt_test_2/config/palette.dart';
+
+class CheckBoxWithLabel extends StatefulWidget {
+  final String label;
+  final double height;
+
+  const CheckBoxWithLabel(
+      {super.key, required this.label, required this.height});
+
+  @override
+  State<CheckBoxWithLabel> createState() => _CheckBoxWithLabelState();
+}
+
+class _CheckBoxWithLabelState extends State<CheckBoxWithLabel> {
+  bool _isChecked = false;
+
+  void onChangeCheck() {
+    setState(() {
+      _isChecked = !_isChecked;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: widget.height,
+      child: Row(
+        children: [
+          Transform.scale(
+            scale: 0.85,
+            child: Checkbox(
+              value: _isChecked,
+              onChanged: (value) {
+                onChangeCheck();
+              },
+              side: MaterialStateBorderSide.resolveWith(
+                (states) => const BorderSide(
+                  width: 1.0,
+                  color: Palette.mint,
+                ),
+              ),
+              activeColor: Palette.white,
+              checkColor: Palette.mint,
+            ),
+          ),
+          Text(
+            widget.label,
+            style: GoogleFonts.nanumGothic(color: Palette.mint, fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/editor/widgets/task_property_window/forms/basic_info_form.dart
+++ lib/editor/widgets/task_property_window/forms/basic_info_form.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_material_symbols/flutter_material_symbols.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ojt_test_2/common/widgets/button.dart';
+import 'package:ojt_test_2/common/widgets/checkbox_with_label.dart';
 import 'package:ojt_test_2/common/widgets/textfield_basic.dart';
 import 'package:ojt_test_2/common/widgets/textfield_with_calendar.dart';
 import 'package:ojt_test_2/common/widgets/textfield_with_icon.dart';
@@ -23,71 +24,85 @@ class BasicInfoForm extends StatelessWidget {
       width: 300,
       padding: const EdgeInsets.all(20),
       decoration: const BoxDecoration(color: Palette.white),
-      child: Expanded(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "TASK NAME",
-                  style: GoogleFonts.nanumGothic(
-                      fontSize: 16, color: Palette.black),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "TASK NAME",
+                style:
+                    GoogleFonts.nanumGothic(fontSize: 16, color: Palette.black),
+              ),
+              const SizedBox(height: 16),
+              TextFieldBasic(label: '태스크명', controller: taskname),
+              const SizedBox(height: 8),
+              TextFieldWithIcon(
+                label: '스케줄',
+                controller: schedule,
+                // icon: const Icon(Icons.search)),
+                icon: const Icon(
+                  MaterialSymbols.more_horiz,
+                  size: 24,
+                  color: Palette.black,
                 ),
-                const SizedBox(height: 16),
-                TextFieldBasic(label: '태스크명', controller: taskname),
-                const SizedBox(height: 8),
-                TextFieldWithIcon(
-                  label: '스케줄',
-                  controller: schedule,
-                  // icon: const Icon(Icons.search)),
-                  icon: const Icon(
-                    MaterialSymbols.more_horiz,
-                    size: 24,
-                    color: Palette.black,
+              ),
+              const SizedBox(height: 8),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextFieldWithCalendar(
+                        label: '시작일',
+                        controller: startDate,
+                        // icon: const Icon(Icons.search)),
+                        icon: const Icon(
+                          MaterialSymbols.calendar_month,
+                          size: 20,
+                          color: Palette.black,
+                        ),
+                      ),
+                      const Text('~'),
+                      TextFieldWithCalendar(
+                        label: '종료일',
+                        controller: endDate,
+                        // icon: const Icon(Icons.search)),
+                        icon: const Icon(
+                          MaterialSymbols.calendar_month,
+                          size: 20,
+                          color: Palette.black,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 8),
-                TextFieldWithCalendar(
-                  label: '시작일',
-                  controller: startDate,
-                  // icon: const Icon(Icons.search)),
-                  icon: const Icon(
-                    MaterialSymbols.calendar_month,
-                    size: 20,
-                    color: Palette.black,
+                  const SizedBox(height: 4),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      CheckBoxWithLabel(label: '작업일기준', height: 32),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 8),
-                TextFieldWithCalendar(
-                  label: '종료일',
-                  controller: endDate,
-                  // icon: const Icon(Icons.search)),
-                  icon: const Icon(
-                    MaterialSymbols.calendar_month,
-                    size: 20,
-                    color: Palette.black,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Button(
-                  width: 80,
-                  height: 24,
-                  text: '저장',
-                  textColor: Palette.white,
-                  textSize: 12,
-                  buttonColor: Palette.mint.withOpacity(0.75),
-                ),
-              ],
-            ),
-          ],
-        ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Button(
+                width: 80,
+                height: 24,
+                text: '저장',
+                textColor: Palette.white,
+                textSize: 12,
+                buttonColor: Palette.mint.withOpacity(0.75),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
