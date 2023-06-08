6234655fefecee76de98c0942f0d68611b7f68c6
savinqkr <savinqkr@gmail.com>
2023-06-08 17:40:08 +0900

    feat: Add Task Widget Menu

--- lib/editor/widgets/editor/diagram_editor/policy/init_policy.dart
+++ lib/editor/widgets/editor/diagram_editor/policy/init_policy.dart
@@ -5,6 +5,6 @@ mixin MyInitPolicy implements InitPolicy {
   @override
   initializeDiagramEditor() {
     canvasWriter.state.setCanvasColor(Colors.white); // Canvas 색상
-    canvasWriter.state.setScale(0.9); // 초기 Scale
+    canvasWriter.state.setScale(1.0); // 초기 Scale
   }
 }
