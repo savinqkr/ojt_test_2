13ae90005c280bbace407710afbace6a800b3f39
kicu-xxo <kicu.xxo@gmail.com>
2023-06-18 23:34:48 +0900

    tab 삭제, 추가 + tree 우클릭 팝업

---
+++ lib/getX/tab_tabs_item_controller.dart
@@ -0,0 +1,36 @@
+import 'package:get/get.dart';
+import 'package:ojt_test_2/common/models/job_data.dart';
+import 'package:ojt_test_2/editor/widgets/editor/tab_menu.dart';
+import 'package:tabbed_view/tabbed_view.dart';
+
+class TabTabsItemController extends GetxController {
+  List<TabData> tabs = <TabData>[].obs;
+  var isOpenJobList = JobData().getOpenJobList();
+
+// tab 초기 렌더링 시 탭 그리는 메서드
+  void addItemsToTabsList() {
+    for (var job in isOpenJobList) {
+      tabs.add(
+        TabData(
+          text: job['name'],
+          keepAlive: true,
+          content: TabMenuContents(jobId: job['id']),
+        ),
+      );
+    }
+    print(tabs);
+    update();
+  }
+
+// 클릭한 작업 tabs 리스트에 추가 메서드
+  void addItemToTabs(item) {
+    tabs.add(
+      TabData(
+        text: item['name'],
+        keepAlive: true,
+        content: TabMenuContents(jobId: item['id']),
+      ),
+    );
+    update();
+  }
+}
