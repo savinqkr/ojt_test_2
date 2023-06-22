70e75cec8a77dabce050f60e62311bee6d21e35b
savinqkr <savinqkr@gmail.com>
2023-06-22 10:11:27 +0900

    feat: Tab click and Render task data

--- lib/editor/widgets/editor/diagram_editor/policy/init_policy.dart
+++ lib/editor/widgets/editor/diagram_editor/policy/init_policy.dart
@@ -8,18 +8,31 @@ import 'package:ojt_test_2/getX/viewing_jobId.controller.dart';
 
 mixin MyInitPolicy implements InitPolicy {
   late String viewingJobId;
+
   @override
   initializeDiagramEditor() {
+    Get.put(ViewingJobIdController());
+
+    // Initialize the diagram editor with the default value
+    viewingJobId = Get.find<ViewingJobIdController>().viewingJobId.value;
+    initializeEditorWithJobId(viewingJobId);
+
+    // Listen to changes in viewingJobId using `ever`
+    ever<String>(Get.find<ViewingJobIdController>().viewingJobId, (value) {
+      // print('>>>>>>>> $value <<<<<<<');
+      canvasWriter.model.removeAllComponents();
+      canvasWriter.model.removeAllLinks();
+      initializeEditorWithJobId(value);
+    });
+  }
+
+  void initializeEditorWithJobId(String jobId) {
     canvasWriter.state.setCanvasColor(Colors.white); // Canvas 색상
     canvasWriter.state.setScale(1.0); // 초기 Scale
 
-    Get.put(ViewingJobIdController());
-    viewingJobId = Get.find<ViewingJobIdController>().viewingJobId;
-
-    var taskList = [];
-
-    if (viewingJobId != '') {
-      var tasks = TaskData().getTaskList(viewingJobId);
+    if (jobId != '') {
+      var taskList = [];
+      var tasks = TaskData().getTaskList(jobId);
 
       tasks.forEach((task) {
         var component = getComponentData(task['position'], task['type']);
@@ -51,38 +64,6 @@ mixin MyInitPolicy implements InitPolicy {
           }
         });
       }
-
-      // var cd2 = getComponentData(const Offset(220, 180), 'schedule');
-      // var cd3 = getComponentData(const Offset(400, 100), 'sleep');
-
-      // canvasWriter.model.addComponent(cd2);
-      // canvasWriter.model.addComponent(cd3);
-
-      // canvasWriter.model.connectTwoComponents(
-      //   sourceComponentId: cd2.id,
-      //   targetComponentId: cd3.id,
-      //   linkStyle: LinkStyle(
-      //     arrowType: ArrowType.pointedArrow,
-      //     lineWidth: 1.5,
-      //     backArrowType: ArrowType.centerCircle,
-      //   ),
-      //   data: MyLinkData(),
-      // );
-      //   // 부모 컴포넌트 앞으로 이동
-      //   canvasWriter.model.moveComponentToTheFront(cd5.id);
-      //   canvasWriter.model.moveComponentToTheFront(cd6.id);
-      //   // 자식 컴포넌트 앞으로 이동
-      //   canvasWriter.model.moveComponentToTheFront(cd1.id);
-      //   canvasWriter.model.moveComponentToTheFront(cd2.id);
-      //   canvasWriter.model.moveComponentToTheFront(cd3.id);
-      //   canvasWriter.model.moveComponentToTheFront(cd4.id);
-
-      //   // 부모-자식 relation 걸기
-      //   canvasWriter.model.setComponentParent(cd1.id, cd5.id);
-      //   canvasWriter.model.setComponentParent(cd2.id, cd5.id);
-      //   // 부모-자식 relation 걸기
-      //   canvasWriter.model.setComponentParent(cd3.id, cd6.id);
-      //   canvasWriter.model.setComponentParent(cd4.id, cd6.id);
     }
   }
 }
