2fff71536ba040185695223a2ab98ca14f83b597
kicu <kicu.xxo@gmail.com>
2023-06-26 18:20:49 +0900

    수정

--- lib/editor/widgets/editor/diagram_editor/policy/link_attachment_policy.dart
+++ lib/editor/widgets/editor/diagram_editor/policy/link_attachment_policy.dart
@@ -10,84 +10,81 @@ mixin MyLinkAttachmentPolicy implements LinkAttachmentPolicy {
   ) {
     Offset pointPosition = targetPoint -
         (componentData.position + componentData.size.center(Offset.zero));
-
     pointPosition = Offset(
       pointPosition.dx / componentData.size.width,
       pointPosition.dy / componentData.size.height,
     );
 
-    PolicySet policySet = PolicySet();
+    switch (componentData.type) {
+      case 'oval':
+        Offset pointAlignment = pointPosition / pointPosition.distance;
 
-    // switch (componentData.type) {
-    //   case 'oval':
-    //     Offset pointAlignment = pointPosition / pointPosition.distance;
+        return Alignment(pointAlignment.dx, pointAlignment.dy);
+      case 'crystal':
+        Offset pointAlignment =
+            pointPosition / (pointPosition.dx.abs() + pointPosition.dy.abs());
 
-    //     return Alignment(pointAlignment.dx, pointAlignment.dy);
-    //   case 'crystal':
-    //     Offset pointAlignment =
-    //         pointPosition / (pointPosition.dx.abs() + pointPosition.dy.abs());
+        return Alignment(pointAlignment.dx, pointAlignment.dy);
 
-    //     return Alignment(pointAlignment.dx, pointAlignment.dy);
-
-    //   default:
-    //     Offset pointAlignment;
-    //     if (pointPosition.dx.abs() >= pointPosition.dy.abs()) {
-    //       pointAlignment =
-    //           Offset(0.0, pointPosition.dy / pointPosition.dx.abs());
-    //     } else {
-    //       pointAlignment =
-    //           Offset(0.0, pointPosition.dy / pointPosition.dy.abs());
-    //     }
-    //     return Alignment(pointAlignment.dx, pointAlignment.dy.roundToDouble());
-    // }
+      default:
+        Offset pointAlignment;
+        if (pointPosition.dx.abs() >= pointPosition.dy.abs()) {
+          pointAlignment = Offset(pointPosition.dx / pointPosition.dx.abs(),
+              pointPosition.dy / pointPosition.dx.abs());
+        } else {
+          pointAlignment = Offset(pointPosition.dx / pointPosition.dy.abs(),
+              pointPosition.dy / pointPosition.dy.abs());
+        }
+        return Alignment(pointAlignment.dx, pointAlignment.dy);
 
 // 수직 정렬이면 component의 상, 하에만 연결, 수평 정렬이면 좌, 우에만 연결
 // 현재는 상, 하만 구현
-    Offset pointAlignment;
-    Alignment alignment;
+//     Offset pointAlignment;
+//     Alignment alignment;
 
-    switch (policySet.getIsAlignVertically()) {
-      //------------------------------------------------------------------
-      case true:
-        if (pointPosition.dx.abs() >= pointPosition.dy.abs()) {
-          pointAlignment =
-              Offset(0.0, pointPosition.dy / pointPosition.dx.abs());
-        } else {
-          pointAlignment =
-              Offset(0.0, pointPosition.dy / pointPosition.dy.abs());
-        }
-        alignment = (pointAlignment.dy == 0.0 || pointAlignment.dy == -0.0)
-            ? const Alignment(0.0, -1.0)
-            : Alignment(pointAlignment.dx, pointAlignment.dy.roundToDouble());
-        return alignment;
-//-------------------------------------------------------------------------
-      case false:
-        if (pointPosition.dx.abs() >= pointPosition.dy.abs()) {
-          pointAlignment =
-              Offset(pointPosition.dx / pointPosition.dx.abs(), 0.0);
-        } else {
-          pointAlignment =
-              Offset(pointPosition.dx / pointPosition.dy.abs(), 0.0);
-        }
-        alignment = (pointAlignment.dx == 0.0 || pointAlignment.dx == -0.0)
-            ? const Alignment(-1.0, 0.0)
-            : Alignment(pointAlignment.dx.roundToDouble(), pointAlignment.dy);
+//     switch (policySet.getIsAlignVertically()) {
+//       //------------------------------------------------------------------
+//       case true:
+//         if (pointPosition.dx.abs() >= pointPosition.dy.abs()) {
+//           pointAlignment =
+//               Offset(0.0, pointPosition.dy / pointPosition.dx.abs());
+//         } else {
+//           pointAlignment =
+//               Offset(0.0, pointPosition.dy / pointPosition.dy.abs());
+//         }
+//         alignment = (pointAlignment.dy == 0.0 || pointAlignment.dy == -0.0)
+//             ? const Alignment(0.0, -1.0)
+//             : Alignment(pointAlignment.dx, pointAlignment.dy.roundToDouble());
+//         return alignment;
+// //-------------------------------------------------------------------------
+//       case false:
+//         if (pointPosition.dx.abs() >= pointPosition.dy.abs()) {
+//           pointAlignment =
+//               Offset(pointPosition.dx / pointPosition.dx.abs(), 0.0);
+//         } else {
+//           pointAlignment =
+//               Offset(pointPosition.dx / pointPosition.dy.abs(), 0.0);
+//         }
+//         alignment = (pointAlignment.dx == 0.0 || pointAlignment.dx == -0.0)
+//             ? const Alignment(-1.0, 0.0)
+//             : Alignment(pointAlignment.dx.roundToDouble(), pointAlignment.dy);
 
-        return alignment;
-// -------------------------------------------------------------------------
-      default:
-        if (pointPosition.dx.abs() >= pointPosition.dy.abs()) {
-          pointAlignment =
-              Offset(0.0, pointPosition.dy / pointPosition.dx.abs());
-        } else {
-          pointAlignment =
-              Offset(0.0, pointPosition.dy / pointPosition.dy.abs());
-        }
-        // 0.0, -0.0은 component의 중간에 연결됨. -> -1.0 값 부여
-        alignment = (pointAlignment.dy == 0.0 || pointAlignment.dy == -0.0)
-            ? const Alignment(0.0, -1.0)
-            : Alignment(pointAlignment.dx, pointAlignment.dy.roundToDouble());
-        return alignment;
+//         return alignment;
+// // -------------------------------------------------------------------------
+//       default:
+//         if (pointPosition.dx.abs() >= pointPosition.dy.abs()) {
+//           pointAlignment =
+//               Offset(0.0, pointPosition.dy / pointPosition.dx.abs());
+//         } else {
+//           pointAlignment =
+//               Offset(0.0, pointPosition.dy / pointPosition.dy.abs());
+//         }
+//         // 0.0, -0.0은 component의 중간에 연결됨. -> -1.0 값 부여
+//         alignment = (pointAlignment.dy == 0.0 || pointAlignment.dy == -0.0)
+//             ? const Alignment(0.0, -1.0)
+//             : Alignment(pointAlignment.dx, pointAlignment.dy.roundToDouble());
+//         return alignment;
+//     }
     }
   }
 }
